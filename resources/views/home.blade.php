8b5b859cb8ff4de57dae895136108e1865633ae7
excelitai-mesaberul <musaberul@excelitai.com>
2021-12-28 16:31:19 +0600

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+@extends('welcome')
+@section('content')
+    <h1>This is home page</h1>
+@endsection
---
+++ resources/views/seyam.blade.php
@@ -0,0 +1,6 @@
+@extends('welcome')
+@section('content')
+<h1>This is Seyam Khan</h1>
+    <h2>{{$name}}</h2>
+@endsection
+
